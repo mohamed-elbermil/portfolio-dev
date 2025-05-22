f930651920e2863a6fc88ad50e7eb9696034fd71
mohamed-elbermil <mohamed-du-69780@hotmail.fr>
2025-05-22 19:08:08 +0100

    création du header

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './styles/_base.css'
 import Header from './components/Header/Header'
 
 function App() {
--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -10,10 +10,15 @@ export default function Header() {
             <p>Disponible pour une opportunité</p>
         </div>
         <img src={Profil} alt="" />
-        <p>Mohamed EL BERMIL</p>
-        <span>Développeur Web</span>
-        <span>France, Lyon</span>
-        <div className="socialMedia"></div>
+        <div className={styles.profile}>
+            <p>Mohamed EL BERMIL</p>
+            <span>Développeur Web</span>
+            <span>France, Lyon</span>
+        </div>
+        <div className={styles.socialMedia}>
+            <i class="fa-brands fa-github"></i>
+            <i class="fa-brands fa-linkedin"></i>
+        </div>
         </>
     )
 }
