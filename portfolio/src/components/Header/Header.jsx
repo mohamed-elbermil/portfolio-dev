5b5907a5792a3830255676bd95525af1a7cb02d5
mohamed-elbermil <mohamed-du-69780@hotmail.fr>
2025-05-22 23:39:13 +0100

    MAJ header

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -7,23 +7,27 @@ export default function Header() {
     return(
         <>
         <Navbar></Navbar>
-        <div className={styles.status}>
-            <div className={styles.alert}>
-                <div className={styles.round}></div>
-                <div className={styles.roundAlert}></div>
+        <header>
+
+            <div className={styles.status}>
+                <div className={styles.alert}>
+                    <div className={styles.round}></div>
+                    <div className={styles.roundAlert}></div>
+                </div>
+                <p>Disponible pour une opportunité</p>
             </div>
-            <p>Disponible pour une opportunité</p>
-        </div>
-        <img src={Profil} alt="" />
-        <div className={styles.profile}>
-            <p>Mohamed EL BERMIL</p>
-            <span>Développeur Web</span>
-            <span>France, Lyon</span>
-        </div>
-        <div className={styles.socialMedia}>
-            <i class="fa-brands fa-github"></i>
-            <i class="fa-brands fa-linkedin"></i>
-        </div>
+            <img src={Profil} alt="" />
+            <div className={styles.profile}>
+                <p>Hey, I’m Mohamed EL BERMIL Web Developer</p>
+                <span>France, Lyon</span>
+                <span>France, LyonFrance, LyonFrance, LyonFrance, LyonFrance, LyonFrance, LyonFrance, LyonFrance, LyonFrance, LyonFrance, Lyon</span>
+                <a href="#">Me contacter</a>
+            </div>
+            <div className={styles.socialMedia}>
+                <i class="fa-brands fa-github"></i>
+                <i class="fa-brands fa-linkedin"></i>
+            </div>
+        </header>
         </>
     )
 }
