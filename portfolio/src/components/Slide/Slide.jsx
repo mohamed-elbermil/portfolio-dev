4a03489ea89e652548bb0044ad788ca21f535fcd
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-05-26 12:25:23 +0200

    ajout de flou

--- portfolio/src/components/Slide/Slide.jsx
+++ portfolio/src/components/Slide/Slide.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Soblim from "../../assets/soblim.png";
 import FitnessArea from "../../assets/fitness-area.png";
+import Perroquet from "../../assets/perroquet.webp";
 import styles from "./Slide.module.css";
 
 export default function Slide() {
@@ -20,6 +21,9 @@ export default function Slide() {
             <span>2023</span>
           </div>
           <img src={FitnessArea} alt="" className={styles.website} />
+        </div>
+        <div className={styles.blurContainer}>
+          <img src={Perroquet} alt="" className={styles.blur} />
         </div>
       </div>
     </>
