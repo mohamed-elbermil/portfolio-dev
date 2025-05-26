36ac52353251ad502d0b8c23f0b809763fc5238d
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-05-26 11:32:09 +0200

    correction resposnsive

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -1,35 +1,36 @@
 import React from "react";
-import Profil from "../../assets/pp.png"
-import styles from "./Header.module.css"
+import Profil from "../../assets/pp.png";
+import styles from "./Header.module.css";
 import Navbar from "../Navbar/Navbar";
-import Button from "../Button/Button"
+import Button from "../Button/Button";
 
 export default function Header() {
-    return(
-        <>
-        <Navbar></Navbar>
-        <header>
-
-            <div className={styles.status}>
-                <div className={styles.alert}>
-                    <div className={styles.round}></div>
-                    <div className={styles.roundAlert}></div>
-                </div>
-                <p>Disponible pour une opportunité</p>
-            </div>
-            <img src={Profil} alt="" />
-            <div className={styles.profile}>
-                <p>Hey, I’m Mohamed EL BERMIL Web Developer</p>
-                <span>France, Lyon</span>
-                <span>France, LyonFrance, LyonFrance, LyonFrance, LyonFrance, LyonFrance, LyonFrance, LyonFrance, LyonFrance, LyonFrance, Lyon</span>
-                    <Button content="Me contacter"></Button>
-                
-            </div>
-            <div className={styles.socialMedia}>
-                <i class="fa-brands fa-github"></i>
-                <i class="fa-brands fa-linkedin"></i>
-            </div>
-        </header>
-        </>
-    )
+  return (
+    <>
+      <Navbar></Navbar>
+      <header>
+        <div className={styles.status}>
+          <div className={styles.alert}>
+            <div className={styles.round}></div>
+            <div className={styles.roundAlert}></div>
+          </div>
+          <p>Disponible pour une opportunité</p>
+        </div>
+        <img src={Profil} alt="" />
+        <div className={styles.profile}>
+          <p>Hey, I’m Mohamed EL BERMIL Web Developer</p>
+          <span>France, Lyon</span>
+          <span>
+            France, LyonFrance, LyonFrance, LyonFrance, LyonFrance, LyonFrance,
+            LyonFrance, LyonFrance, LyonFrance, LyonFrance, Lyon
+          </span>
+          <Button content="Me contacter"></Button>
+        </div>
+        <div className={styles.socialMedia}>
+          <i class="fa-brands fa-github"></i>
+          <i class="fa-brands fa-linkedin"></i>
+        </div>
+      </header>
+    </>
+  );
 }
--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,32 @@
 import React from "react";
-import styles from "./Navbar.module.css"
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-    return(
-        <>
-        <nav>
-            <ul>
-                <a href="#" className={styles.round}>
-                    <i class="fa-solid fa-house"></i>
-                </a>
+  return (
+    <>
+      <nav>
+        <ul>
+          <a href="#" className={styles.round}>
+            <i class="fa-solid fa-house"></i>
+          </a>
 
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Projets</a></li>
-                <li><a href="#">A propos</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </nav>
-        </>
-    )
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Entreprise</a>
+          </li>
+          <li>
+            <a href="#">Veille technologique</a>
+          </li>
+          <li>
+            <a href="#">Projets</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
