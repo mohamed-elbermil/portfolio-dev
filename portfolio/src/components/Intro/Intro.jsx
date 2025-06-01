a65facdfa27097a73eac2668dbbc9194537287e8
mohamed-elbermil <mohamed-du-69780@hotmail.fr>
2025-06-01 12:29:04 +0100

    Découverte de l'alternance

--- portfolio/src/components/Intro/Intro.jsx
+++ portfolio/src/components/Intro/Intro.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import Title from '../Title/Title';
 import { section } from 'framer-motion/client';
+import styles from "./Intro.module.css"
 
 export default function Intro() {
   return (
     <>
-    <section>
+    <section className={styles.context}>
         <Title
-                theme="Outils"
+                theme="Contexte"
                     title={
                         <>
                         Le contexte de l' <span className='span'>&lt;Alternance /&gt;</span>
                         </>
                             }
                             text="Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum."
-                            />
-        <ul>
-            <li>Mon rôle : Intégrateur Web</li>
-            <li>Rythme d’alternance : 2 jours d’école et 3 jours entreprises</li>
+                            /><br></br>
+        <ul className={styles.list}>
+            <li><span className='primary'><i class="fa-solid fa-person"></i></span> Intégrateur Web</li>
+            <li><span className='primary'><i class="fa-solid fa-clock"></i></span> 2 jours d’école et 3 jours entreprises</li>
+
+            <li><span className='primary'><i class="fa-solid fa-briefcase"></i></span> Alternance</li>
         </ul>
 
     </section>
